b20b5b8184bb87436b4b52d5687f43a14578cc06
Ian McIntyre <ianpmcintyre@gmail.com>
2020-05-09 11:39:47 -0400

    Add sensible VMAs and LMAs
    LMAs were RAM, but VMAs were flash. Switched those up so it doesn't
    look strange.

--- src/bin/render.rs
+++ src/bin/render.rs
@@ -13,11 +13,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     ls.stack(ram.clone()).unwrap();
     ls.heap(ram.clone()).unwrap();
     ls.boot_config(512, "fcb", flash.clone()).unwrap();
-    ls.vector_table(flash.clone(), Some(ram.clone())).unwrap();
-    ls.text(flash.clone(), Some(ram.clone())).unwrap();
-    ls.data(false, flash.clone(), Some(ram.clone())).unwrap();
+    ls.vector_table(ram.clone(), Some(flash.clone())).unwrap();
+    ls.text(flash.clone(), None).unwrap();
+    ls.data(false, ram.clone(), Some(flash.clone())).unwrap();
     ls.rodata(false, flash.clone(), None).unwrap();
-    ls.bss(false, flash.clone(), Some(ram.clone())).unwrap();
+    ls.bss(false, ram.clone(), Some(flash.clone())).unwrap();
     ls.write(&mut io::stdout().lock())
         .map_err(|err| Box::new(err) as _)
 }
