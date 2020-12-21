fd81149a1ec725791dfb0c92b8b4320da4bfc4fc
Tom Burdick <thomas.burdick@gmail.com>
2020-12-21 14:02:58 -0600

    Work towards generating a reset handler
    This requires overriding the Reset linker section of cortex-m-rt
    possibly, or simply copying it over like we've done before

--- src/generate/reset.rs
+++ src/generate/reset.rs
@@ -1,7 +1,13 @@
 use crate::{LinkerScript, Word};
-use std::io::Error;
+use std::io::{Error, Write};
 
 /// Generate a reset module from a LinkerScript
-pub fn render<W: Word>(_ls: &LinkerScript<W>) -> Result<Vec<u8>, Error> {
-    Ok(Vec::new())
+pub fn render<W: Word, Wr: Write>(out: &mut Wr,
+                                  linker_script: &LinkerScript<W>
+) -> Result<(), Error> {
+    writeln!(out, "#[doc(Hidden)]");
+    writeln!(out, "#[link_section = \".vector_table.reset_vector\"]");
+    writeln!(out, "#[no_mangle]");
+    writeln!(out, "pub static __RESET_VECTOR: unsafe extern \"C\" fn() -> ! = Reset;");
+    Ok(())
 }
